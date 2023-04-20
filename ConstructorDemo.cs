bd5b7a87f8745028245101e1f2d3d74ef30d4952
EMMA <69660913+Emma2410@users.noreply.github.com>
2023-04-20 15:39:26 +0630

    Add files via upload
    Static and Non-Static Constuctor

---
+++ ConstructorDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ConstructorDemo
+    {
+        static int y;
+        int x;
+
+        static ConstructorDemo() {
+
+            Console.WriteLine("Stastic Constructor is called");
+
+        }
+        public ConstructorDemo()
+        {
+            Console.WriteLine("Non_stastic Constructor is called");
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Main Method is called");
+
+            ConstructorDemo d1 = new ConstructorDemo();
+            Console.WriteLine(y);  // stastic variable is called directly
+           // Console.WriteLine(x); /// Error can occur
+            Console.WriteLine(d1.x); /// Error can occur
+            Console.ReadLine();
+
+        }
+
+    }
+}
