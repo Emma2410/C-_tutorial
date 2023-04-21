ff51d5bc2ad98b4b7ce2bd0de3f4068ab26d7f75
EMMA <69660913+Emma2410@users.noreply.github.com>
2023-04-21 13:05:02 +0630

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access1
+{
+    public class Program
+    {
+
+        private void Test1()
+        {
+            Console.WriteLine("Private");
+        }
+        internal void Test2()
+        {
+            Console.WriteLine("Internal");
+        }
+         protected void Test3()
+        {
+            Console.WriteLine("Protected");
+        }
+        protected internal void Test4()
+        {
+            Console.WriteLine("Protected Internal");
+        }
+        public void Test5()
+        {
+            Console.WriteLine("Public Method");
+        }
+        static void Main(string[] args)
+        {
+            Program p  = new Program();
+            p.Test1(); p.Test2(); p.Test3(); p.Test4();
+            
+        }
+    }
+}
