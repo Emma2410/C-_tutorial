dd3f647a15db283fa4f4b9742121be89885eb4b8
EMMA <69660913+Emma2410@users.noreply.github.com>
2023-04-21 13:06:01 +0630

    Add files via upload

---
+++ Four.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDemo2
+{
+    class Four: Access1.Program
+    {
+        static void Main()
+        {
+            Four f = new Four();
+            //f.Test2();
+            f.Test3(); f.Test4();
+            Console.ReadLine();
+        }
+    }
+}
