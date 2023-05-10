7c9fc1ae6fdbe6b2e8c395e30382790b78ff88cf
EMMA <69660913+Emma2410@users.noreply.github.com>
2023-05-10 10:17:20 +0630

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Abstruction_ex
+namespace Access1
 {
-    internal class Program
+    public class Program
     {
-        static void Main()
+
+        private void Test1()
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Private");
+        }
+        internal void Test2()
+        {
+            Console.WriteLine("Internal");
+        }
+         protected void Test3()
+        {
+            Console.WriteLine("Protected");
+        }
+        protected internal void Test4()
+        {
+            Console.WriteLine("Protected Internal");
+        }
+        public void Test5()
+        {
+            Console.WriteLine("Public Method");
+        }
+        static void Main(string[] args)
+        {
+            Program p  = new Program();
+            p.Test1(); p.Test2(); p.Test3(); p.Test4();
+            
         }
     }
 }
---
+++ Two.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access1
+{
+    class Two: Program
+    {
+        static void Main()
+        {
+            Two t = new Two();
+            t.Test2(); t.Test3();
+            t.Test4();
+        }
+    }
+}
