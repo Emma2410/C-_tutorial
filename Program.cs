5d4588c7b19964b7fef512dc7f36bd65fbbede5d
EMMA <69660913+Emma2410@users.noreply.github.com>
2023-05-10 10:17:45 +0630

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,36 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Access1
+namespace AccessDemo1
 {
     public class Program
     {
-
         private void Test1()
         {
-            Console.WriteLine("Private");
+            Console.WriteLine("Private Mode is  called");
         }
-        internal void Test2()
-        {
-            Console.WriteLine("Internal");
+        Internal Test2()
+        { 
+            Console.WriteLine("Internal method");
         }
-         protected void Test3()
-        {
-            Console.WriteLine("Protected");
-        }
-        protected internal void Test4()
-        {
-            Console.WriteLine("Protected Internal");
-        }
-        public void Test5()
-        {
-            Console.WriteLine("Public Method");
-        }
+
         static void Main(string[] args)
         {
-            Program p  = new Program();
-            p.Test1(); p.Test2(); p.Test3(); p.Test4();
-            
         }
     }
 }
