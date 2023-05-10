57edb39f07b02db0ffc7a2df292f79424b00d2e3
EMMA <69660913+Emma2410@users.noreply.github.com>
2023-05-10 10:16:18 +0630

    Add files via upload

---
+++ Interface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstruction
+{
+    interface IAnimal
+    {
+        void animalSound();
+
+    } 
+    class Pig: IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The Pig says: Wee Wee");
+        }
+
+      
+    }
+    internal class Interface
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.animalSound();
+            
+        }
+    }
+
+   
+}
